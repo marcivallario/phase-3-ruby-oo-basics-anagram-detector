13a1170be441d58cd9ef256239e71b94276e0ac3
Marci Vallario <marcivallario@gmail.com>
2022-01-16 15:15:53 -0500

    commit tree 4fc6bfcdda3da25e475a40c103ac41f8b60de95c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+    def initialize string
+        @string = string
+    end
+
+    def match arr
+        result = arr.filter {|str| @string.chars.sort == str.chars.sort}
+    end
+end
+
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
